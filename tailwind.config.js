9bf8734cee545462d45f83edf90318275ae63e41
Andrey Bogdanov <andrey@wonder.shop>
2020-09-12 09:42:30 +0300

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/*
+** TailwindCSS Configuration File
+**
+** Docs: https://tailwindcss.com/docs/configuration
+** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
+*/
+module.exports = {
+  important: true,
+  theme: {},
+  variants: {},
+  plugins: [],
+  future: {
+    removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true
+  },
+  experimental: {
+    applyComplexClasses: true
+  }
+}
